103bb7da8347f2ac676463be736b657a91283b03
Jan Stavěl @ RedHat <jstavel@redhat.com>
2017-02-14 10:09:05 +0100

    new version.

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject com.redhat.qe/bugzilla-testng "1.0.4"
+(defproject com.redhat.qe/bugzilla-testng "1.1.0"
   :description "Bugzilla integration for TestNG tests - check if a bz is open before running a test."
   :java-source-path "src"
-  :dependencies [[com.redhat.qe/bz-checker "1.0.2"]
+  :dependencies [[com.redhat.qe/bz-checker "2.0.3-SNAPSHOT"]
                  [com.redhat.qe/jul.test.records "1.0.1"]
                  [org.testng/testng "6.0.1"]
                  [com.redhat.qe/logging-testscript "1.0.2"]]
-  :javac-options {:debug "on"})
+  :javac-options {:debug "on"}
+  :plugins [[lein2-eclipse "2.0.0"]])
