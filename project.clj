2d18046bcc6506c605af7e801ad6de95661d8f5f
Jan Stavěl @ RedHat <jstavel@redhat.com>
2017-02-14 16:08:13 +0100

    SNAPSHOT in version is back

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject com.redhat.qe/bugzilla-testng "1.1.0"
+(defproject com.redhat.qe/bugzilla-testng "1.1.2-SNAPSHOT"
   :description "Bugzilla integration for TestNG tests - check if a bz is open before running a test."
   :java-source-path "src"
-  :dependencies [[com.redhat.qe/bz-checker "2.0.3"]
+  :dependencies [[com.redhat.qe/bz-checker "2.0.3-SNAPSHOT"]
                  [com.redhat.qe/jul.test.records "1.0.1"]
                  [org.testng/testng "6.0.1"]
                  [com.redhat.qe/logging-testscript "1.0.2"]]
