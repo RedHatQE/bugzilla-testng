b8570c6554e7a6330014f06d95e3fd5aee369603
Jan Stavěl @ RedHat <jstavel@redhat.com>
2017-02-16 21:24:54 +0100

    fixed project.clj to use lein2

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject com.redhat.qe/bugzilla-testng "1.1.3-SNAPSHOT"
   :description "Bugzilla integration for TestNG tests - check if a bz is open before running a test."
   :java-source-path "src"
+  :java-source-paths ["src"]
   :dependencies [[com.redhat.qe/bz-checker "2.0.3-SNAPSHOT"]
                  [com.redhat.qe/jul.test.records "1.0.1"]
                  [org.testng/testng "6.0.1"]
